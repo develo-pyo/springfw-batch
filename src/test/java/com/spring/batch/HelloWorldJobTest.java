e6123cb6402d08bb7aaa1df31f9202a9024512cd
develo-pyo <jungpyo9@gmail.com>
2020-02-24 22:23:51 +0900

    first commit

---
+++ src/test/java/com/spring/batch/HelloWorldJobTest.java
@@ -0,0 +1,16 @@
+package com.spring.batch;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "/batch/job/hello_world_job.xml" })
+public class HelloWorldJobTest {
+
+//    @Autowired
+//    private JobLauncherTestUtils jobLauncherTestUtils;
+//
+//    @Test
+//    public void testJob() throws Exception {
+//        final JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+//        System.out.println("!!!!!!213123sdfasdf");
+//        Assert.assertEquals(ExitStatus.COMPLETED.getExitCode(), jobExecution.getExitStatus().getExitCode());
+//    }
+}
